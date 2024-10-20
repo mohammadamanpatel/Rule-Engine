5dd6f3ce48594b98e94b92575277a885b9f519ab
Mohd Aman Naim Patel <ap5277478@gmail.com>
2024-10-20 19:21:07 +0530

    finalizing-project

--- rule-engine-frontend/vite.config.js
+++ rule-engine-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // Replace with your backend URL
+        target: 'http://localhost:8000', 
         changeOrigin: true,
         secure: false,
       },
